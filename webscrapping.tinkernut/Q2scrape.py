74bad49b87658366f403cab989631f3a7d44050a
Vollenhoven <95358171+Vollenhoven@users.noreply.github.com>
2023-05-29 13:24:09 +0200

    Add files via upload
    commit

---
+++ webscrapping.tinkernut/Q2scrape.py
@@ -0,0 +1,19 @@
+
+from bs4 import BeautifulSoup
+import requests 
+import pandas as pd
+import csv
+
+page_to_scrape = requests.get('http://quotes.toscrape.com')
+soup = BeautifulSoup(page_to_scrape.text,'html.parser')
+quotes = soup.findAll('span', attrs={ 'class' :'text'})
+authors = soup.findAll('small', attrs={ 'class' :'author'})
+
+file = open('scrapped_quotes2.csv', 'w')
+writer = csv.writer(file)
+writer.writerow(['QUOTES','AUTHORS'])
+
+for quote,author in zip(quotes,authors):
+        print(quote.text + '-' + author.text)
+        writer.writerow([quote.text,author.text])
+file.close()
